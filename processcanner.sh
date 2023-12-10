3e3d6de069c13788fac61fc82243c5fea22fdc04
joao-hg-marques <81370082+joao-hg-marques@users.noreply.github.com>
2023-12-10 17:57:03 +0000

    Add files via upload

---
+++ processcanner.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Insert VirusTotal API key
+API_KEY="d135d84b91c39055da8672aa30ce5a2f5a31d8e1961bb5bad08c8208956c8e36"
+
+# This gets the list of running processes
+PROCESSES=$(ps -eo command)
+
+# Initialize counters
+total=0
+malicious=0
+no_malicious=0
+
+# Loop through the processes do loop
+while read -r process; do
+  # Hash the process using sha256
+  hash=$(echo "$process" | sha256sum | awk '{print $1}')
+
+  # It uses VirusTotal API to check the hash
+  response=$(curl -s -X POST "https://www.virustotal.com/api/v3/files/$hash/behaviour" \
+    -H "x-apikey: $API_KEY")
+
+  # Check the response for the "detected_communicating_samples" field. This will communicate VT and Endpoint (data base for malware analysis.
+  detected=$(echo "$response" | jq -r '.data.attributes.detected_communicating_samples | length')
+
+  # Print the process and detection status and prints a report
+  if [ "$detected" -gt "0" ]; then
+    echo "Process $process is potentially malicious ($detected detections)"
+    ((malicious++))
+  else
+    echo "Process $process is not malicious"
+    ((no_malicious++))
+  fi
+  ((total++))
+done <<< "$PROCESSES"
+
+# Print the total number of scanned files
+echo "Total number of scanned files: $total"
+echo "Number of potentially malicious files: $malicious"
+echo "Number of no malicious files: $no_malicious"
+
